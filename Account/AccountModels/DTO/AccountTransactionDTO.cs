f531b004fc229784d7c1953447a8f3c4b6cd2bec
eliassemaan9 <78341540+eliassemaan9@users.noreply.github.com>
2023-10-15 21:42:34 +0300

    Backenf-Task new Endpoint ADDED

---
+++ Account/AccountModels/DTO/AccountTransactionDTO.cs
@@ -0,0 +1,22 @@
+﻿using AccountModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountModels.DTO
+{
+    public class AccountTransactionDTO
+    {
+
+        public long customerId { get; set; }
+
+        public decimal? balance { get; set; }
+
+        public int? type { get; set; }
+
+        public long accountId { get; set; }
+        public List<Transaction> transactions { get; set; }
+    }
+}
---
+++ Account/AccountModels/DTO/AccountsInfoDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountModels.DTO
+{
+    public class AccountsInfoDTO
+    {
+        public long customerId { get; set; }
+        public string name { get; set; }
+        public List<AccountTransactionDTO> account { get; set; }
+    }
+}
